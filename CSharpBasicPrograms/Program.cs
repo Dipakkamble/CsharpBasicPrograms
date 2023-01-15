e2fbad87278e523ed10ae8e1de112d9fad55b515
Dipakkamble <kambledipak700@gmail.com>
2023-01-15 16:02:13 +0530

    [Dipak] Add Switchcase concept

--- CSharpBasicPrograms/Program.cs
+++ CSharpBasicPrograms/Program.cs
@@ -11,6 +11,9 @@ namespace CSharpBasicPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome csharp basic programs");
+            Console.WriteLine("Pleaase enter the day number between 1 to 7");
+            int day=Convert.ToInt32(Console.ReadLine());
+            WeekDay.FindDayWhichFallOnWeek(day);
             Console.ReadLine();
         }
     }
